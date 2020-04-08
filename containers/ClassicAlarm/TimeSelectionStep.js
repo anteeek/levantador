20d624d2a191b6a94eb7ec415ee67d2cbda839e2
antekpapieski <maciejpapiewski9@gmail.com>
2020-04-08 22:54:22 +0200

    cleaned package.json

--- containers/ClassicAlarm/TimeSelectionStep.js
+++ containers/ClassicAlarm/TimeSelectionStep.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Appearance } from "react-native";
 import { Text } from "react-native-paper";
 
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
 export default () => {
+
     return (
         <View>
-            <Text>
-                heheheehe
-            </Text>
+            <DateTimePickerModal
+                isVisible={true}
+                isDarkModeEnabled={true}
+                mode="time"
+                onConfirm={console.log}
+                onCancel={console.log}
+            />
         </View>
     )
 }
